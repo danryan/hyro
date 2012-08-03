92da86ffc495de32a17b55d3b3816f96f6db3a21
Michael Juster <maj@crowdflower.com>
2012-08-03 14:17:15 -0700

    handle nil time conversions

--- lib/hyro/transform/time.rb
+++ lib/hyro/transform/time.rb
@@ -3,10 +3,12 @@ module Hyro
     class Time
       
       def self.decode(v)
+        return nil if v.nil?
         ::DateTime.strptime(v, "%Y-%m-%dT%H:%M:%SZ").to_time
       end
       
       def self.encode(v)
+        return nil if v.nil?
         v.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
       end
       
--- spec/transform/time_spec.rb
+++ spec/transform/time_spec.rb
@@ -1,15 +1,23 @@
 describe Hyro::Transform::Time do
-  
+
   describe ".encode" do
     it "should emit ISO-8601" do
       Hyro::Transform::Time.encode(Time.utc(2012, 1, 25, 10, 20, 33)).should == '2012-01-25T10:20:33Z'
     end
+
+    it "should handle nil" do
+      Hyro::Transform::Time.encode(nil).should == nil
+    end
   end
-  
+
   describe ".decode" do
     it "should accept ISO-8601" do
       Hyro::Transform::Time.decode('2012-01-25T10:20:33Z').should == Time.utc(2012, 1, 25, 10, 20, 33)
     end
+
+    it "should handle nil" do
+      Hyro::Transform::Time.decode(nil).should == nil
+    end
   end
-  
+
 end
