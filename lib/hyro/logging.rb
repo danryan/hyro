56ea404909b549c16496227189d11ac983ddce9d
Mars Hall <mars@crowdflower.com>
2012-08-08 17:05:02 -0700

    Log status 'FAILED' when connection refused.

--- lib/hyro/logging.rb
+++ lib/hyro/logging.rb
@@ -8,11 +8,11 @@ module Hyro
       Kernel.const_defined?(:ActiveSupport) && ActiveSupport.const_defined?(:Notifications)
       
       ActiveSupport::Notifications.subscribe('request.faraday') do |name, start_time, end_time, _, env|
-        url = env[:url]
-        status = env[:response].status
+        url = env[:url].to_s
+        status = env[:response] ? env[:response].status.to_s : 'FAILED'
         http_method = env[:method].to_s.upcase
         duration = ((end_time - start_time) * 1000)
-        Rails.logger.info 'Hyro %s %s %d (%.1fms)' % [http_method, url.to_s, status, duration]
+        Rails.logger.info 'Hyro %s %s %s (%.1fms)' % [http_method, url, status, duration]
       end
     end
     
