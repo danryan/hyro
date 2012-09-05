e895b763a2bb74eca2ee3c8a77c078f7cb8aa901
Mars Hall <mars@crowdflower.com>
2012-09-05 13:57:46 -0700

    Update code comments.

--- lib/hyro/configuration.rb
+++ lib/hyro/configuration.rb
@@ -6,7 +6,7 @@ module Hyro
     attr_accessor :base_path # /widgets or /admin/widgets
     attr_accessor :auth_type # "Bearer"
     attr_accessor :auth_token # "SEKRETTOKEN"
-    attr_accessor :actions # a hash of action names => HTTP methods
+    attr_accessor :actions # a nested hash of scope (collection|member|new) => action names => HTTP method
     attr_accessor :transforms # a hash of attributes names => transformer classes which define :encode & :decode
   end
 end
